eecbbb497e446780f316f7c97cb3ab72ecc49a85
Danish007 <38866352+Danish-uddin@users.noreply.github.com>
2023-09-03 21:38:26 -0400

    first commit

---
+++ week_02/nbad_assignment_2/index3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>HTML: Hello world</p>
+    <h2>
+        <?php 
+            echo "PHP code inside html: ";
+            echo 9 * 13;
+        ?>
+    </h2>
+</body>
+</html>
